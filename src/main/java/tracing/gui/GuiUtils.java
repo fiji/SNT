fe11f3f34d84ba161d247bf02ccfee1b4430988e
Tiago <tiagoalvespedrosa@gmail.com>
2017-09-07 13:35:08 -0400

    UI: implement pop-up messages

--- src/main/java/tracing/gui/GuiUtils.java
+++ src/main/java/tracing/gui/GuiUtils.java
@@ -24,11 +24,20 @@ package tracing.gui;
 
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 
+import javax.swing.JDialog;
 import javax.swing.JFileChooser;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
+import javax.swing.Timer;
+import javax.swing.border.EmptyBorder;
 
 import org.scijava.ui.swing.SwingDialog;
 import org.scijava.ui.swing.widget.SwingColorWidget;
@@ -62,6 +71,56 @@ public class GuiUtils {
 
 	public void error(final String msg, final String title, final boolean icon) {
 		simpleMsg(msg, title, icon ? JOptionPane.ERROR_MESSAGE : JOptionPane.PLAIN_MESSAGE);
+	public void tempMsg(final String msg, final boolean snapToParent) {
+		tempMsg(msg, -1, -1, snapToParent);
+	}
+
+	public void tempMsg(final String msg, final Point location) {
+		tempMsg(msg, location.x, location.y, false);
+	}
+
+	public void tempMsg(final String msg, final int x, final int y) {
+		tempMsg(msg, x, y, false);
+	}
+
+	private void tempMsg(final String msg, final int x, final int y,
+		final boolean snapToParent)
+	{
+		final JDialog dialog = new JDialog();
+		dialog.setUndecorated(true);
+		dialog.setModal(false);
+		dialog.getContentPane().setBackground(Color.WHITE);
+		dialog.setBackground(Color.WHITE);
+		final JLabel label = new JLabel(msg);
+		label.setHorizontalAlignment(SwingConstants.CENTER);
+		label.setBorder(new EmptyBorder(10, 10, 10, 10));
+		label.setBackground(Color.WHITE);
+		dialog.add(label);
+		dialog.pack();
+		if (snapToParent && parent != null) {
+			dialog.setPreferredSize(new Dimension(parent.getWidth(), dialog
+				.getHeight()));
+			final Point p = new Point(parent.getWidth() / 2 - dialog.getWidth() / 2,
+				parent.getHeight() / 2 - dialog.getHeight() / 2);
+			dialog.setLocation(p.x + parent.getX(), p.y + parent.getY());
+		}
+		else if (x != -1 && y != -1) {
+			dialog.setLocation(x, y);
+		}
+		else {
+			dialog.setLocationRelativeTo(null);
+		}
+		final Timer timer = new Timer(2500, new ActionListener() {
+
+			@Override
+			public void actionPerformed(final ActionEvent e) {
+				dialog.setVisible(false);
+				dialog.dispose();
+			}
+		});
+		timer.setRepeats(false);
+		timer.start();
+		dialog.setVisible(true);
 	}
 
 	public int yesNoDialog(final String msg, final String title) {

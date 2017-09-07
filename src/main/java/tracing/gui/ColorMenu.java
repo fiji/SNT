6f9ce695832fe03a069785e440cfc5ed16071b64
Tiago <tiagoalvespedrosa@gmail.com>
2017-09-07 13:39:51 -0400

    Revamp ColorMenu to use SWCcolors, make swatches customizable via right-click

--- src/main/java/tracing/gui/ColorMenu.java
+++ src/main/java/tracing/gui/ColorMenu.java
@@ -5,9 +5,7 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
-import java.awt.Insets;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.ArrayList;
@@ -15,24 +13,29 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JPanel;
+import javax.swing.MenuElement;
 import javax.swing.MenuSelectionManager;
+import javax.swing.SwingUtilities;
 import javax.swing.border.Border;
 import javax.swing.border.CompoundBorder;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.MatteBorder;
 
+import tracing.Path;
+
 /**
- * This class generates a simplified color widget. It is based on Gerald Bauer's
- * code released under GPL2
+ * This class generates a simplified color widget holding both pre-determined
+ * colors, user-defined ones and defaults for SWC types. It is based on Gerald
+ * Bauer's code released under GPL2
  * (http://www.java2s.com/Code/Java/Swing-JFC/ColorMenu.htm)
  */
 public class ColorMenu extends JMenu {
 
 	private static final long serialVersionUID = 1L;
-	private final Map<Color, ColorPane> _colorPanes;
+	private final Map<SWCColor, ColorPane> _colorPanes;
 	private ColorPane _selectedColorPane;
 	private final Border _activeBorder;
 	private final Border _selectedBorder;
@@ -45,20 +48,18 @@ public class ColorMenu extends JMenu {
 			getBackground()), new MatteBorder(1, 1, 1, 1, getForeground()));
 
 		_selectedBorder = new CompoundBorder(new MatteBorder(1, 1, 1, 1,
-			getBackground()), new MatteBorder(2, 2, 2, 2, getForeground()));
+			getForeground().brighter()), new MatteBorder(2, 2, 2, 2,
+				getForeground()));
 
 		_activeBorder = new CompoundBorder(new MatteBorder(2, 2, 2, 2,
-			getForeground()), new MatteBorder(1, 1, 1, 1, getBackground()));
+			getBackground().darker()), new MatteBorder(1, 1, 1, 1, getBackground()));
 
 		final Color[] hues = new Color[] { Color.RED, Color.GREEN, Color.BLUE,
 			Color.MAGENTA, Color.CYAN, Color.YELLOW, Color.ORANGE };
 		final float[] colorRamp = new float[] { .75f, .5f, .3f };
 		final float[] grayRamp = new float[] { 1, .86f, .71f, .57f, .43f, .29f, 0 };
 
-		final JPanel p = new JPanel();
-		p.setBackground(getBackground());
-		p.setBorder(new EmptyBorder(5, 5, 5, 5));
-		p.setLayout(new GridLayout(8, 8));
+		final JPanel defaultPanel = getGridPanel(8, 7);
 		_colorPanes = new HashMap<>();
 
 		final List<Color> colors = new ArrayList<>();
@@ -82,53 +83,74 @@ public class ColorMenu extends JMenu {
 			colors.add(color);
 		}
 		for (final Color color : colors) {
-			final ColorPane colorPane = new ColorPane(color);
-			p.add(colorPane);
-			_colorPanes.put(color, colorPane);
+			final ColorPane colorPane = new ColorPane(new SWCColor(color), false);
+			defaultPanel.add(colorPane);
+			_colorPanes.put(new SWCColor(color), colorPane);
 		}
-		add(p);
+		add(defaultPanel);
 
-		// Build the custom color panel
-		final JPanel cPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 5, 0));
-		cPanel.setBackground(getBackground());
-		final ColorPane customColorPane = new ColorPane(getBackground());
-		cPanel.add(customColorPane);
-		_colorPanes.put(getBackground(), customColorPane);
-		final JButton promptButton = new JButton("Other...");
-		promptButton.setMargin(new Insets(0, 0, 0, 0));
-		promptButton.setContentAreaFilled(false);
-		promptButton.setBorderPainted(false);
-		promptButton.setOpaque(false);
-		promptButton.addActionListener(new ActionListener() {
+		// Build the custom color row
+		addSeparator("Custom... (Righ-click to change):");
+		final JPanel customPanel = getGridPanel(1, 7);
+		for (int i = 0; i < 7; i++) {
+			final Color uniquePlaceHolderColor = new Color(getBackground().getRed(),
+				getBackground().getGreen(), getBackground().getBlue(), 255 - i - 1);
+			final ColorPane customColorPane = new ColorPane(new SWCColor(
+				uniquePlaceHolderColor), true);
+			customPanel.add(customColorPane);
+			_colorPanes.put(new SWCColor(uniquePlaceHolderColor), customColorPane);
+		}
+		add(customPanel);
 
-			@Override
-			public void actionPerformed(final ActionEvent e) {
-				final Color c = new GuiUtils(getTopLevelAncestor()).getColor(
-					"New Color", getColor());
-				customColorPane.setColor(c);
-				setColor(c);
-				doSelection();
-			}
-		});
-		cPanel.add(promptButton);
-		add(cPanel);
+		// Build the panel for SWC colors
+		addSeparator("SWC Type Colors (Righ-click to change):");
+		final JPanel swcPanel = getGridPanel(1, 7);
+		for (final int type : Path.getSWCtypes()) {
+			final SWCColor swcColor = new SWCColor(Path.getSWCcolor(type), type);
+			final ColorPane swcColorPane = new ColorPane(swcColor, true);
+			swcPanel.add(swcColorPane);
+			_colorPanes.put(swcColor, swcColorPane);
+		}
+		add(swcPanel);
 		addSeparator();
 	}
 
-	public void setColor(final Color c) {
+	private void addSeparator(final String title) {
+		final JPanel panelLabel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		panelLabel.setBorder(new EmptyBorder(4, 0, 0, 0));
+		panelLabel.setBackground(getBackground());
+		final JLabel label = new JLabel(title);
+		label.setFont(getFont().deriveFont((float) (getFont().getSize() / 1.5)));
+		panelLabel.add(label);
+		add(panelLabel);
+	}
+
+	private JPanel getGridPanel(final int rows, final int cols) {
+		final JPanel panel = new JPanel();
+		panel.setBackground(getBackground());
+		panel.setBorder(new EmptyBorder(0, 0, 0, 0));
+		panel.setLayout(new GridLayout(rows, cols));
+		return panel;
+	}
+
+	public void selectSWCColor(final SWCColor c) {
 		final Object obj = _colorPanes.get(c);
-		if (obj == null) return;
+		if (obj == null) {
+			selectNone();
+			return;
+		}
 		if (_selectedColorPane != null) _selectedColorPane.setSelected(false);
 		_selectedColorPane = (ColorPane) obj;
 		_selectedColorPane.setSelected(true);
 	}
 
-	public Color getColor() {
+	public SWCColor getSelectedSWCColor() {
 		if (_selectedColorPane == null) return null;
-		return _selectedColorPane.getColor();
+		return _selectedColorPane.swcColor;
+	}
 	}
 
-	public void doSelection() {
+	private void doSelection() {
 		fireActionPerformed(new ActionEvent(this, ActionEvent.ACTION_PERFORMED,
 			getActionCommand()));
 	}
@@ -136,31 +158,37 @@ public class ColorMenu extends JMenu {
 	private class ColorPane extends JPanel implements MouseListener {
 
 		private static final long serialVersionUID = 1L;
-		private Color _color;
-		private boolean _isSelected;
+		private SWCColor swcColor;
+		private boolean isSelectected;
+		private final boolean isCustomizable;
 
-		public ColorPane(final Color color) {
-			setColor(color);
+		public ColorPane(final SWCColor sColor, final boolean customizable) {
+			swcColor = sColor;
+			isCustomizable = customizable;
+			setPanelSWCColor(swcColor);
 			setBorder(_unselectedBorder);
 			addMouseListener(this);
 		}
 
-		public void setColor(final Color color) {
-			_color = color;
-			setBackground(color);
-			final String msg = "RGB: " + color.getRed() + ", " + color.getGreen() +
-				", " + color.getBlue();
+		private void setPanelSWCColor(final SWCColor sColor) {
+			swcColor = sColor;
+			setBackground(swcColor.color());
+			final String msg = (swcColor.type() == SWCColor.SWC_TYPE_IGNORED)
+				? "RGB: " + swcColor.color().getRed() + ", " + swcColor.color()
+					.getGreen() + ", " + swcColor.color().getBlue() : Path.getSWCtypeName(
+						swcColor.type());
 			setToolTipText(msg);
 		}
 
 		public void setSelected(final boolean isSelected) {
-			_isSelected = isSelected;
-			if (_isSelected) setBorder(_selectedBorder);
-			else setBorder(_unselectedBorder);
-		}
-
-		public Color getColor() {
-			return _color;
+			isSelectected = isSelected;
+			if (isSelectected) {
+				setBorder(_selectedBorder);
+				_selectedColorPane = this;
+			}
+			else {
+				setBorder(_unselectedBorder);
+			}
 		}
 
 		@Override
@@ -188,15 +216,53 @@ public class ColorMenu extends JMenu {
 
 		@Override
 		public void mouseExited(final MouseEvent ev) {
-			setBorder(_unselectedBorder);
+			setBorder(isSelectected ? _selectedBorder : _unselectedBorder);
 		}
 
 		@Override
-		public void mousePressed(final MouseEvent ev) {}
+		public void mousePressed(final MouseEvent ev) {
+			if (isCustomizable && (SwingUtilities.isRightMouseButton(ev) || ev
+				.isPopupTrigger()))
+			{
+				ev.consume();
+
+				// Remember menu path so that it can be restored after prompt
+				final MenuElement[] path = MenuSelectionManager.defaultManager()
+					.getSelectedPath();
+
+				// Ensure this colorPane gets selected
+				selectNone();
+				setSelected(true);
+
+				// Prompt user for new color
+				final GuiUtils gUtils = new GuiUtils(getTopLevelAncestor());
+				final String promptTitle = (swcColor
+					.type() == SWCColor.SWC_TYPE_IGNORED) ? "New Color"
+						: "New color for SWC Type: " + Path.getSWCtypeName(swcColor.type());
+				final Color c = gUtils.getColor(promptTitle, swcColor.color());
+				if (c == null) return; // user pressed cancel
+				if (c.equals(swcColor.color())) return; // nothing to change
+
+				if (_colorPanes.containsKey(new SWCColor(c, swcColor.type()))) {
+					gUtils.tempMsg("Chosen color already listed in palette!", ev
+						.getLocationOnScreen());
+					return;
+				}
+
+				// Otherwise refresh panel with new choice
+				swcColor.setAWTColor(c);
+				setPanelSWCColor(swcColor);
+				doSelection();
+
+				// restore menu path. Alert user if choice is invalid
+				MenuSelectionManager.defaultManager().setSelectedPath(path);
+
+			}
+		}
 
 		@Override
 		public void mouseReleased(final MouseEvent ev) {
-			setColor(_color);
+			selectSWCColor(swcColor);
 			MenuSelectionManager.defaultManager().clearSelectedPath();
 			doSelection();
 		}
@@ -207,11 +273,18 @@ public class ColorMenu extends JMenu {
 		final javax.swing.JFrame f = new javax.swing.JFrame();
 		final javax.swing.JMenuBar menuBar = new javax.swing.JMenuBar();
 		final ColorMenu menu = new ColorMenu("Test");
+		menu.addActionListener(new java.awt.event.ActionListener() {
+
+			@Override
+			public void actionPerformed(final ActionEvent e) {
+				System.out.println(e);
+				System.out.println(menu.getSelectedSWCColor());
+			}
+		});
 		menuBar.add(menu);
 		f.setJMenuBar(menuBar);
+		f.pack();
 		f.setVisible(true);
-		menuBar.setVisible(true);
-		menu.setVisible(true);
 	}
 
 }

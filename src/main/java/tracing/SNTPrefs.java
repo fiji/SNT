7a194d6d58fc01febedf1914249e67d49d3a3f78
Tiago <tiagoalvespedrosa@gmail.com>
2017-09-19 01:21:33 -0400

    Remove GUI methods from SNTPrefs. These are now implemented in the plugins Jtabbed pane

--- src/main/java/tracing/SNTPrefs.java
+++ src/main/java/tracing/SNTPrefs.java
@@ -22,35 +22,33 @@
 
 package tracing;
 
-import java.awt.Font;
 import java.awt.Point;
 
 import tracing.FillWindow;
 import ij.Prefs;
-import ij.gui.GenericDialog;
 
 /**
  * Class handling SNT preferences.
  *
  * @author Tiago Ferreira
  */
-public class SNTPrefs {
+public class SNTPrefs { //TODO: Adopt PrefService
 
 	private static final int DRAW_DIAMETERS_XY = 1;
 	private static final int SNAP_CURSOR = 2;
 	// private static final int SHOW_MIP_OVERLAY = 4;
 	private static final int AUTO_CANVAS_ACTIVATION = 8;
-	private static final int ENFORCE_LUT = 16;
+	//@Deprecated//private static final int ENFORCE_LUT = 16;
 	private static final int USE_THREE_PANE = 32;
 	private static final int USE_3D_VIEWER = 64;
-	private static final int LOOK_FOR_TUBES = 128;
-	private static final int LOOK_FOR_OOF = 256;
+	//@Deprecated//private static final int LOOK_FOR_TUBES = 128;
+	//@Deprecated//private static final int LOOK_FOR_OOF = 256;
 	private static final int SHOW_ONLY_SELECTED = 512;
 	private static final int STORE_WIN_LOCATIONS = 1024;
-	// private static final int JUST_NEAR_SLICES = 1024;
+	//@Deprecated//private static final int JUST_NEAR_SLICES = 1024;
 	private static final int ENFORCE_DEFAULT_PATH_COLORS = 2048;
 	private static final int DEBUG = 4096;
-	private static final int LOOK_FOR_TRACES = 8192;
+	//@Deprecated//private static final int LOOK_FOR_TRACES = 8192;
 	private static final int COMPRESSED_XML = 16384;
 
 	private static final String BOOLEANS = "tracing.snt.booleans";
@@ -59,27 +57,22 @@ public class SNTPrefs {
 	private static final String PATHWIN_LOC = "tracing.snt.pwloc";
 	private static final String FILLWIN_LOC = "tracing.snt.fwloc";
 
-	//private static final String SNAP_Z = "tracing.snt.zsnap";
-
-	// private final static String NEARBY_VIEW = "tracing.snt.nearbyview";
-
 	private final int UNSET_PREFS = -1;
 	private int currentBooleans;
-	private final Simple_Neurite_Tracer snt;
+	private final SimpleNeuriteTracer snt;
 
-	public SNTPrefs(final Simple_Neurite_Tracer snt) {
+	public SNTPrefs(final SimpleNeuriteTracer snt) {
 		this.snt = snt;
 		getBooleans();
 	}
 
-	public SNTPrefs(final SimpleNeuriteTracer snt) {
-		this.snt = (Simple_Neurite_Tracer) snt;
+	public SNTPrefs(final Simple_Neurite_Tracer snt) {
+		this.snt = (SimpleNeuriteTracer) snt;
 		getBooleans();
 	}
 
 	private int getDefaultBooleans() {
-		return DRAW_DIAMETERS_XY + SNAP_CURSOR + AUTO_CANVAS_ACTIVATION + USE_THREE_PANE + LOOK_FOR_TUBES + LOOK_FOR_OOF
-				+ LOOK_FOR_TRACES + COMPRESSED_XML;
+		return DRAW_DIAMETERS_XY + SNAP_CURSOR + AUTO_CANVAS_ACTIVATION + COMPRESSED_XML;
 	}
 
 	private void getBooleans() {
@@ -97,7 +90,7 @@ public class SNTPrefs {
 		snt.drawDiametersXY = getPref(DRAW_DIAMETERS_XY);
 		snt.displayCustomPathColors = !getPref(ENFORCE_DEFAULT_PATH_COLORS);
 		snt.setShowOnlySelectedPaths(getPref(SHOW_ONLY_SELECTED), false);
-		SimpleNeuriteTracer.verbose = getPref(DEBUG);
+		SNT.setDebugMode(getPref(DEBUG));
 		snt.cursorSnapWindowXY = (int) Prefs.get(SNAP_XY, 4);
 		snt.cursorSnapWindowXY = whithinBoundaries(snt.cursorSnapWindowXY,
 				SimpleNeuriteTracer.MIN_SNAP_CURSOR_WINDOW_XY, SimpleNeuriteTracer.MAX_SNAP_CURSOR_WINDOW_XY);
@@ -116,11 +109,12 @@ public class SNTPrefs {
 		return value;
 	}
 
+	@Deprecated
 	protected void loadStartupPrefs() {
-		snt.forceGrayscale = getPref(ENFORCE_LUT);
-		snt.look4oofFile = getPref(LOOK_FOR_OOF);
-		snt.look4tubesFile = getPref(LOOK_FOR_TUBES);
-		snt.look4tracesFile = getPref(LOOK_FOR_TRACES);
+		//snt.forceGrayscale = getPref(ENFORCE_LUT);
+		//snt.look4oofFile = getPref(LOOK_FOR_OOF);
+		//snt.look4tubesFile = getPref(LOOK_FOR_TUBES);
+		//snt.look4tracesFile = getPref(LOOK_FOR_TRACES);
 		snt.setSinglePane(!getPref(USE_THREE_PANE));
 		snt.use3DViewer = getPref(USE_3D_VIEWER);
 	}
@@ -129,12 +123,9 @@ public class SNTPrefs {
 		return (currentBooleans & key) != 0;
 	}
 
+	@Deprecated
 	protected void saveStartupPrefs() {
-		setPref(ENFORCE_LUT, snt.forceGrayscale);
-		setPref(LOOK_FOR_OOF, snt.look4oofFile);
-		setPref(LOOK_FOR_TUBES, snt.look4tubesFile);
-		setPref(LOOK_FOR_TRACES, snt.look4tracesFile);
-		setPref(USE_THREE_PANE, !snt.getSinglePane());
+		//setPref(USE_THREE_PANE, !snt.getSinglePane());
 		setPref(USE_3D_VIEWER, snt.use3DViewer);
 		Prefs.set(BOOLEANS, currentBooleans);
 		Prefs.savePreferences();
@@ -149,7 +140,7 @@ public class SNTPrefs {
 		setPref(DRAW_DIAMETERS_XY, snt.drawDiametersXY);
 		setPref(ENFORCE_DEFAULT_PATH_COLORS, !snt.displayCustomPathColors);
 		setPref(SHOW_ONLY_SELECTED, snt.showOnlySelectedPaths);
-		setPref(DEBUG, SimpleNeuriteTracer.verbose);
+		setPref(DEBUG, SNT.isDebugMode());
 		Prefs.set(BOOLEANS, currentBooleans);
 		clearLegacyPrefs();
 		if (isSaveWinLocations()) {
@@ -169,6 +160,10 @@ public class SNTPrefs {
 		return getPref(STORE_WIN_LOCATIONS);
 	}
 
+	protected void setSaveWinLocations(final boolean value) {
+		setPref(STORE_WIN_LOCATIONS, value);
+	}
+
 	private void setPref(final int key, final boolean value) {
 		if (value)
 			currentBooleans |= key;
@@ -184,7 +179,7 @@ public class SNTPrefs {
 		return Prefs.getLocation(FILLWIN_LOC);
 	}
 
-	private void resetOptions() {
+	protected void resetOptions() {
 		clearLegacyPrefs();
 		Prefs.set(BOOLEANS, null);
 		Prefs.set(SNAP_XY, null);
@@ -196,97 +191,6 @@ public class SNTPrefs {
 
 	private void clearLegacyPrefs() {
 		Prefs.set("tracing.Simple_Neurite_Tracer.drawDiametersXY", null);
-	}
-
-	protected void promptForOptions() {
-
-		final int startupOptions = 7;
-		final int pluginOptions = 2;
-
-		final String[] startupLabels = new String[startupOptions];
-		final int[] startupItems = new int[startupOptions];
-		final boolean[] startupStates = new boolean[startupOptions];
-		int idx = 0;
-
-		startupItems[idx] = ENFORCE_LUT;
-		startupLabels[idx] = "Enforce non-inverted grayscale LUT";
-		startupStates[idx++] = snt.forceGrayscale;
-
-		startupItems[idx] = USE_THREE_PANE;
-		startupLabels[idx] = "Use_three-pane view";
-		startupStates[idx++] = !snt.getSinglePane();
-
-		startupItems[idx] = USE_3D_VIEWER;
-		startupLabels[idx] = "Use_3D Viewer";
-		startupStates[idx++] = snt.use3DViewer;
-
-		startupItems[idx] = LOOK_FOR_TUBES;
-		startupLabels[idx] = "Load_Tubeness \".tubes.tif\" pre-processed file (if present)";
-		startupStates[idx++] = snt.look4tubesFile;
-
-		startupItems[idx] = LOOK_FOR_OOF;
-		startupLabels[idx] = "Load_Tubular_Geodesics \".oof.ext\" pre-processed file (if present)";
-		startupStates[idx++] = snt.look4oofFile;
-
-		startupItems[idx] = LOOK_FOR_TRACES;
-		startupLabels[idx] = "Load_default \".traces\" file (if present)";
-		startupStates[idx++] = snt.look4tracesFile;
-
-		startupItems[idx] = STORE_WIN_LOCATIONS;
-		startupLabels[idx] = "Remember window locations across restarts";
-		startupStates[idx++] = isSaveWinLocations();
-
-		final String[] pluginLabels = new String[pluginOptions];
-		final int[] pluginItems = new int[pluginOptions];
-		final boolean[] pluginStates = new boolean[pluginOptions];
-		idx = 0;
-
-		pluginItems[idx] = COMPRESSED_XML;
-		pluginLabels[idx] = "Use compression when saving traces";
-		pluginStates[idx++] = snt.useCompressedXML;
-
-		pluginItems[idx] = DEBUG;
-		pluginLabels[idx] = "Enable_debug mode";
-		pluginStates[idx++] = SimpleNeuriteTracer.verbose;
-
-		final GenericDialog gd = new GenericDialog("SNT v" + SNT.VERSION + " Preferences");
-		final Font font = new Font("SansSerif", Font.BOLD, 12);
-		gd.setInsets(0, 0, 0);
-		gd.addMessage("Startup Options:", font);
-		gd.setInsets(0, 0, 0);
-		gd.addCheckboxGroup(startupOptions, 1, startupLabels, startupStates);
-		gd.setInsets(20, 0, 0);
-		gd.addMessage("Advanced Options:", font);
-		gd.setInsets(0, 0, 0);
-		gd.addCheckboxGroup(pluginOptions, 1, pluginLabels, pluginStates);
-
-		gd.enableYesNoCancel("OK", "Revert to Defaults");
-		gd.showDialog();
-		if (gd.wasCanceled()) {
-			return;
-		} else if (gd.wasOKed()) {
-
-			for (int i = 0; i < startupOptions; i++) {
-				if (gd.getNextBoolean())
-					currentBooleans |= startupItems[i];
-				else
-					currentBooleans &= ~startupItems[i];
-			}
-			for (int i = 0; i < pluginOptions; i++) {
-				if (gd.getNextBoolean())
-					currentBooleans |= pluginItems[i];
-				else
-					currentBooleans &= ~pluginItems[i];
-			}
-			Prefs.set(BOOLEANS, currentBooleans);
-
-		} else {
-			resetOptions();
-		}
-
-		Prefs.savePreferences();
-		loadPluginPrefs();
-
 	}
 
 }

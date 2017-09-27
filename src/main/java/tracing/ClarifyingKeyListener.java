f49081fe549dd1df8d65afffdddf039022457328
Tiago <tiagoalvespedrosa@gmail.com>
2017-09-27 14:04:06 -0400

    Code cleanup

--- src/main/java/tracing/ClarifyingKeyListener.java
+++ src/main/java/tracing/ClarifyingKeyListener.java
@@ -8,12 +8,12 @@
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
@@ -120,11 +120,10 @@ class ClarifyingKeyListener implements KeyListener, ContainerListener {
 	}
 
 	@Override
-	public void keyReleased(final KeyEvent e) {
-	}
+	public void keyReleased(final KeyEvent e) {}
 
 	@Override
-	public void keyTyped(final KeyEvent e) {
+	public void keyTyped(final KeyEvent e) {}
 
 	private boolean isDoublePress(final KeyEvent ke) {
 		if (lastKeyPressedCode == ke.getKeyCode() && ((ke.getWhen() -
@@ -133,5 +132,4 @@ class ClarifyingKeyListener implements KeyListener, ContainerListener {
 		lastKeyPressedCode = ke.getKeyCode();
 		return false;
 	}
-
 }
--- src/main/java/tracing/QueueJumpingKeyListener.java
+++ src/main/java/tracing/QueueJumpingKeyListener.java
@@ -8,12 +8,12 @@
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
@@ -39,9 +39,11 @@ class QueueJumpingKeyListener implements KeyListener {
 	private static final int DOUBLE_PRESS_INTERVAL = 300; // ms
 	private long timeKeyDown = 0; // last time key was pressed
 	private int lastKeyPressedCode;
-	private boolean mac;
+	private final boolean mac;
 
-	public QueueJumpingKeyListener(final SimpleNeuriteTracer tracerPlugin, final InteractiveTracerCanvas canvas) {
+	public QueueJumpingKeyListener(final SimpleNeuriteTracer tracerPlugin,
+		final InteractiveTracerCanvas canvas)
+	{
 		this.tracerPlugin = tracerPlugin;
 		this.canvas = canvas;
 		mac = PlatformUtils.isMac();
@@ -50,7 +52,7 @@ class QueueJumpingKeyListener implements KeyListener {
 	@Override
 	public void keyPressed(final KeyEvent e) {
 
-		if (!tracerPlugin.isReady())
+		if (!tracerPlugin.isReady()) // false if plugin.getResultsDialog() == null
 			return;
 
 		final int keyCode = e.getKeyCode();
@@ -58,47 +60,49 @@ class QueueJumpingKeyListener implements KeyListener {
 		final boolean doublePress = isDoublePress(e);
 
 		final boolean shift_pressed = (keyCode == KeyEvent.VK_SHIFT);
-		final boolean join_modifier_pressed = mac ? keyCode == KeyEvent.VK_ALT : keyCode == KeyEvent.VK_CONTROL;
+		final boolean join_modifier_pressed = mac ? keyCode == KeyEvent.VK_ALT
+			: keyCode == KeyEvent.VK_CONTROL;
 
 		final int modifiers = e.getModifiersEx();
 		final boolean shift_down = (modifiers & InputEvent.SHIFT_DOWN_MASK) > 0;
 		final boolean control_down = (modifiers & InputEvent.CTRL_DOWN_MASK) > 0;
 		final boolean alt_down = (modifiers & InputEvent.ALT_DOWN_MASK) > 0;
 
-		SNT.debug("keyCode=" + keyCode + " (" + KeyEvent.getKeyText(keyCode) + ") keyChar=\"" + keyChar + "\" ("
-					+ (int) keyChar + ") " + KeyEvent.getKeyModifiersText(canvas.getModifiers()));
+		SNT.debug("keyCode=" + keyCode + " (" + KeyEvent.getKeyText(keyCode) +
+			") keyChar=\"" + keyChar + "\" (" + (int) keyChar + ") " + KeyEvent
+				.getKeyModifiersText(canvas.getModifiers()));
 
-		if (keyChar == 'y' || keyChar == 'Y') {
-			if (tracerPlugin.resultsDialog.finishOnDoubleConfimation && doublePress) //FIXME: May throw a NPE if resultsDialog closed
-				tracerPlugin.finishedPath();
-			else
-				tracerPlugin.confirmTemporary();
 		if (keyCode == KeyEvent.VK_ENTER) {
 			tracerPlugin.getResultsDialog().toFront();
 			e.consume();
 		}
 
-		} else if (keyCode == KeyEvent.VK_ESCAPE && tracerPlugin.resultsDialog !=  null) {
-
-			if (doublePress)
-				tracerPlugin.resultsDialog.reset();
-			else
-				tracerPlugin.resultsDialog.abortCurrentOperation();
+		else if (keyChar == 'y' || keyChar == 'Y') {
+			if (tracerPlugin.getResultsDialog().finishOnDoubleConfimation &&
+				doublePress) tracerPlugin.finishedPath();
+			else tracerPlugin.confirmTemporary();
 			e.consume();
+		}
 
-		} else if (keyChar == 'n' || keyChar == 'N') {
-			if (tracerPlugin.resultsDialog.discardOnDoubleCancellation && doublePress) //FIXME: May throw a NPE if resultsDialog closed
-				tracerPlugin.cancelPath();
-			else
-				tracerPlugin.cancelTemporary();
+		else if (keyCode == KeyEvent.VK_ESCAPE) {
+			if (doublePress) tracerPlugin.getResultsDialog().reset();
+			else tracerPlugin.getResultsDialog().abortCurrentOperation();
 			e.consume();
+		}
 
-		} else if (keyChar == 'c' || keyChar == 'C') {
+		else if (keyChar == 'n' || keyChar == 'N') {
+			if (tracerPlugin.getResultsDialog().discardOnDoubleCancellation &&
+				doublePress) tracerPlugin.cancelPath();
+			else tracerPlugin.cancelTemporary();
+			e.consume();
+		}
 
+		else if (keyChar == 'c' || keyChar == 'C') {
 			tracerPlugin.cancelPath();
 			e.consume();
+		}
 
-		} else if (keyChar == 'f' || keyChar == 'F') {
+		else if (keyChar == 'f' || keyChar == 'F') {
 
 			// if (verbose) SNT.log( "Finalizing that path" );
 			tracerPlugin.finishedPath();
@@ -110,28 +114,30 @@ class QueueJumpingKeyListener implements KeyListener {
 //			tracerPlugin.makePathVolume();
 //			e.consume();
 
-		} else if (keyChar == '5') {
+		}
 
+		else if (keyChar == '5') {
 			canvas.toggleJustNearSlices();
 			tracerPlugin.updateViewPathChoice();
 			e.consume();
+		}
 
-		} else if (keyChar == 'm' || keyChar == 'M') {
-
+		else if (keyChar == 'm' || keyChar == 'M') {
 			canvas.clickAtMaxPoint();
 			e.consume();
+		}
 
-		} else if (keyChar == 's' || keyChar == 's') {
-
+		else if (keyChar == 's' || keyChar == 's') {
 			tracerPlugin.toogleSnapCursor();
 			e.consume();
+		}
 
-		} else if (keyChar == 'g' || keyChar == 'G') {
-
+		else if (keyChar == 'g' || keyChar == 'G') {
 			canvas.selectNearestPathToMousePointer(shift_down || control_down);
 			e.consume();
+		}
 
-		} else if (shift_pressed || join_modifier_pressed) {
+		else if (shift_pressed || join_modifier_pressed) {
 
 			/*
 			 * This case is just so that when someone starts holding down the
@@ -143,14 +149,15 @@ class QueueJumpingKeyListener implements KeyListener {
 			e.consume();
 		}
 
-		if (shift_down && (control_down || alt_down) && (keyCode == KeyEvent.VK_A)) {
+		if (shift_down && (control_down || alt_down) &&
+			(keyCode == KeyEvent.VK_A))
+		{
 			canvas.startShollAnalysis();
 			e.consume();
 		}
 
 		for (final KeyListener kl : listeners) {
-			if (e.isConsumed())
-				break;
+			if (e.isConsumed()) break;
 			kl.keyPressed(e);
 		}
 
@@ -159,8 +166,7 @@ class QueueJumpingKeyListener implements KeyListener {
 	@Override
 	public void keyReleased(final KeyEvent e) {
 		for (final KeyListener kl : listeners) {
-			if (e.isConsumed())
-				break;
+			if (e.isConsumed()) break;
 			kl.keyReleased(e);
 		}
 	}
@@ -168,13 +174,12 @@ class QueueJumpingKeyListener implements KeyListener {
 	@Override
 	public void keyTyped(final KeyEvent e) {
 		for (final KeyListener kl : listeners) {
-			if (e.isConsumed())
-				break;
+			if (e.isConsumed()) break;
 			kl.keyTyped(e);
 		}
 	}
 
-	private boolean isDoublePress(KeyEvent ke) {
+	private boolean isDoublePress(final KeyEvent ke) {
 		if (lastKeyPressedCode == ke.getKeyCode() && ((ke.getWhen() -
 			timeKeyDown) < DOUBLE_PRESS_INTERVAL)) return true;
 		timeKeyDown = ke.getWhen();
@@ -183,12 +188,13 @@ class QueueJumpingKeyListener implements KeyListener {
 	}
 
 	/**
-	 * This method should add the other key listeners in 'laterKeyListeners'
-	 * that will be called for 'source' if this key listener isn't interested in
-	 * the key press.
+	 * This method should add the other key listeners in 'laterKeyListeners' that
+	 * will be called for 'source' if this key listener isn't interested in the
+	 * key press.
 	 */
 	public void addOtherKeyListeners(final KeyListener[] laterKeyListeners) {
-		final ArrayList<KeyListener> newListeners = new ArrayList<>(Arrays.asList(laterKeyListeners));
+		final ArrayList<KeyListener> newListeners = new ArrayList<>(Arrays.asList(
+			laterKeyListeners));
 		listeners.addAll(newListeners);
 	}
 

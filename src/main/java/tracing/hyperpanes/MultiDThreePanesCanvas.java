4cefcb25126e1321064d35abbfed9b4c8012edb0
Tiago <tiagoalvespedrosa@gmail.com>
2017-12-06 03:13:57 +0000

    Bugfix: Graphics object not being reused While at it, use antialised text by default

--- src/main/java/tracing/hyperpanes/MultiDThreePanesCanvas.java
+++ src/main/java/tracing/hyperpanes/MultiDThreePanesCanvas.java
@@ -107,8 +107,9 @@ public class MultiDThreePanesCanvas extends ImageCanvas {
 	}
 
 	protected Graphics2D getGraphics2D(final Graphics g) {
-		final Graphics2D g2 = (Graphics2D) getGraphics();
+		final Graphics2D g2 = (Graphics2D) g;
 		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
 		return g2;
 	}
 

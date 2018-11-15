e0c44390ef5d199d395153b8cb91538e9c71f63e
Tiago Ferreira <tiago.tferr@gmail.com>
2018-11-15 16:24:23 -0500

    Add Loader for FlyCircuit

---
+++ src/main/java/tracing/io/FlyCirCuitLoader.java
@@ -0,0 +1,134 @@
+/*-
+ * #%L
+ * Fiji distribution of ImageJ for the life sciences.
+ * %%
+ * Copyright (C) 2010 - 2018 Fiji developers.
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+package tracing.io;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import tracing.PathAndFillManager;
+import tracing.Tree;
+
+/**
+ * Absurdly simple importer for retrieving SWC data from
+ * <a href="http://www.flycircuit.tw">FlyCircuit</a>.
+ * 
+ * @author Tiago Ferreira
+ */
+public class FlyCirCuitLoader {
+
+	private static final String BASE_URL = "http://www.flycircuit.tw/download/swc/";
+
+	/**
+	 * Checks whether a connection to the FlyCircuit database can be established.
+	 *
+	 * @return true, if an HHTP connection could be established, false otherwise
+	 */
+	public boolean isDatabaseAvailable() {
+		HttpURLConnection connection = null;
+		boolean isOnline = false;
+		try {
+			final URL url = new URL(BASE_URL);
+			connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod("GET");
+			connection.setConnectTimeout(5000);
+			connection.connect();
+			isOnline = connection.getResponseCode() == HttpURLConnection.HTTP_FORBIDDEN;
+		} catch (final IOException ignored) {
+			// do nothing
+		} finally {
+			connection.disconnect();
+		}
+		return isOnline;
+	}
+
+	/**
+	 * Gets the URL of the SWC file associated with the specified cell ID.
+	 *
+	 * @param cellId the ID of the cell to be retrieved
+	 * @return the reconstruction URL
+	 */
+	public String getReconstructionURL(final String cellId) {
+		final StringBuilder sb = new StringBuilder();
+		sb.append(BASE_URL).append(cellId.replaceAll(" ", "%20"));
+		if (!cellId.endsWith(".swc"))
+			sb.append(".swc");
+		return sb.toString();
+	}
+
+	/**
+	 * Gets the SWC data associated with the specified cell ID as a reader
+	 *
+	 * @param cellId the ID of the cell to be retrieved
+	 * @return the character stream containing the data, or null if cell ID was not
+	 *         found or could not be retrieved
+	 */
+	public BufferedReader getReader(final String cellId) {
+		try {
+			final URL url = new URL(getReconstructionURL(cellId));
+			final InputStream is = url.openStream();
+			return new BufferedReader(new InputStreamReader(is));
+		} catch (final IOException e) {
+			return null;
+		}
+	}
+
+	/**
+	 * Gets the collection of Paths for the specified cell ID
+	 *
+	 * @param cellId the ID of the cell to be retrieved
+	 * @return the data for the specified cell as a {@link Tree}, or null if data
+	 *         could not be retrieved
+	 */
+	public Tree getTree(final String cellId) {
+		final PathAndFillManager pafm = new PathAndFillManager();
+		pafm.setHeadless(true);
+		if (pafm.importSWC(getReconstructionURL(cellId))) {
+			final Tree tree = new Tree(pafm.getPaths());
+			tree.setLabel(cellId);
+			return tree;
+		}
+		return null;
+	}
+
+	/*
+	 * IDE debug method
+	 *
+	 * @throws IOException
+	 */
+	public static void main(final String... args) {
+		final String cellId = "VGlut-F-400787";
+		final FlyCirCuitLoader loader = new FlyCirCuitLoader();
+		final Tree tree = loader.getTree(cellId);
+
+		System.out.println("# Getting neuron " + cellId);
+		System.out.println("FlyCircuit available: " + loader.isDatabaseAvailable());
+		System.out.println("URL :" + loader.getReconstructionURL(cellId));
+		System.out.println("Successul import: " + (tree != null));
+		System.out.println(cellId + " contains  " + tree.size() + " paths");
+		System.out.println("# All done");
+	}
+}

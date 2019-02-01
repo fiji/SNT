93a3a55c1fbd3bf28a663f6d9dc9dba68609a5ad
Tiago Ferreira <tiago.tferr@gmail.com>
2019-02-01 15:23:54 -0500

    Add boilerplate templates to the script editor

---
+++ src/main/resources/script_templates/Neuroanatomy/Boilerplate/GVY.groovy
@@ -0,0 +1,32 @@
+#@Context context
+#@DatasetService ds
+#@DisplayService display
+#@ImageJ ij
+#@LegacyService ls
+#@LogService log
+#@LUTService lut
+#@SNTService snt
+#@StatusService status
+#@UIService ui
+
+
+/**
+ * file:	
+ * author:	
+ * version:	
+ * info:	
+ */
+
+
+import tracing.Path
+import tracing.PathAndFillManager
+import tracing.SimpleNeuriteTracer
+import tracing.SNTUI
+import tracing.Tree
+import tracing.analysis.*
+import tracing.analysis.sholl.*
+import tracing.io.*
+import tracing.plugin.*
+import tracing.util.*
+import tracing.viewer.*
+

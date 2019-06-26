0c23cd4a85822f5291a842bc4809448cd5a1edb4
Tiago Ferreira <tiago.tferr@gmail.com>
2019-06-26 08:06:51 -0400

    Standardize boilerplate scripts

--- src/main/resources/script_templates/Neuroanatomy/Boilerplate/PY.py
+++ src/main/resources/script_templates/Neuroanatomy/Boilerplate/PY.py
@@ -1,29 +1,29 @@
-# @Context context
-# @DatasetService ds
-# @DisplayService display
-# @ImageJ ij
-# @LegacyService ls
-# @LogService log
-# @LUTService lut
-# @SNTService snt
-# @StatusService status
-# @UIService ui
+#@Context context
+#@DatasetService ds
+#@DisplayService display
+#@ImageJ ij
+#@LegacyService ls
+#@LogService log
+#@LUTService lut
+#@SNTService snt
+#@UIService ui
 
 
 """
-file:
-author:
-version:
-info:
+file:    
+version: 
+info:    
 """
 
 from sc.fiji.snt import (Path, PathAndFillManager, SNT, SNTUI, Tree)
-from sc.fiji.snt.analysis import (RoiConverter, MultiTreeColorMapper, TreeAnalyzer,
-        TreeColorMapper, TreeStatistics)
+from sc.fiji.snt.analysis import (MultiTreeColorMapper, PathProfiler, RoiConverter,
+        TreeAnalyzer, TreeColorMapper, TreeStatistics)
+from sc.fiji.snt.analysis.graph import GraphUtils
 from sc.fiji.snt.analysis.sholl import TreeParser
+from sc.fiji.snt.annotation import (AllenCompartment, AllenUtils, VFBUtils, ZBAtlasUtils)
 from sc.fiji.snt.io import (FlyCircuitLoader, MouseLightLoader, NeuroMorphoLoader)
 from sc.fiji.snt.plugin import (SkeletonizerCmd, StrahlerCmd)
 from sc.fiji.snt.util import (BoundingBox, PointInImage, SNTColor, SWCPoint)
-from sc.fiji.snt.viewer import (OBJMesh, Viewer2D, Viewer3D)
-from sc.fiji.snt.viewer.Viewer3D import ViewMode
+from sc.fiji.snt.viewer import (Annotation3D, OBJMesh, Viewer2D, Viewer3D)
 
+# See https://javadoc.scijava.org/Fiji/index.html?sc/fiji/snt/package-summary.html

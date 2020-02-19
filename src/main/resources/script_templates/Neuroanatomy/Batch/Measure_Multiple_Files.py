abeb94ac1cb019f4acfadf3cc62756bb82b8cd4a
Arshadi <arshadic@hhmi.org>
2020-02-19 15:35:02 -0500

    fix Measure_Multiple_Files.py

--- src/main/resources/script_templates/Neuroanatomy/Batch/Measure_Multiple_Files.py
+++ src/main/resources/script_templates/Neuroanatomy/Batch/Measure_Multiple_Files.py
@@ -10,10 +10,10 @@
 
 
 """
-file:       Measure_Multiple_Files.py
-author:     Tiago Ferreira
-version:    20180531
-info:       Bulk measurements of reconstruction files using SNT
+file:	   Measure_Multiple_Files.py
+author:	 Tiago Ferreira
+version:	20180531
+info:	   Bulk measurements of reconstruction files using SNT
 """
 
 import os, sys
@@ -23,56 +23,50 @@ from org.scijava.table import DefaultGenericTable
 
 
 
-def get_swc_files(directory, filtering_string):
-    """Returns a list containing the paths of files in the specified
-       directory. The list will only include SWC, TRACES and JSON
-       files whose filename contains the specified string."""
-    if not recursive:
-        return Tree.listFromDir(directory, filtering_string)
-    files = []
-    for (dirpath, dirnames, filenames) in os.walk(directory):
-        for subdir in dirnames:
-            files.append(Tree.listFromDir(directory, filtering_string)
-    return files
+def get_trees(directory, filtering_string):
+	"""Returns a list containing the Tree objects which represent
+	   reconstruction files in the specified directory. The list will 
+	   only contain Trees from SWC, TRACES and JSON files whose filename contains 
+	   the specified string."""
+	if not recursive:
+		return Tree.listFromDir(directory, filtering_string)
+	trees = []
+	for (dirpath, dirnames, filenames) in os.walk(directory):
+			trees += Tree.listFromDir(dirpath, filtering_string)
+			
+	return trees
 
 
 def run():
 
-    d = str(input_dir)
-    files = get_swc_files(d, name_filter)
-    if not files or len(files) == 0:
-        uiservice.showDialog("No files matched the specified criteria", "Error")
-        return
+	d = str(input_dir)
+	trees = get_trees(d, name_filter)
+	if not trees or len(trees) == 0:
+		uiservice.showDialog("No files matched the specified criteria", "Error")
+		return
 
-    # Define a common table to host results
-    table = DefaultGenericTable()
+	# Define a common table to host results
+	table = DefaultGenericTable()
 
-    for (counter, f) in enumerate(files):
+	for (counter, t) in enumerate(trees):
 
-        basename = os.path.basename(f)
-        msg = 'Loading file %s: %s...' % (counter + 1, basename)
-        status.showStatus(msg)
-        log.info(msg)
+		msg = 'Analyzing Tree: %s: %s...' % (counter + 1, t.getLabel())
+		status.showStatus(msg)
+		log.info(msg)
 
-        # Import file as an analyzable Tree. Move on if import failed
-        tree = Tree.fromFile(f)
-        if not tree:
-           log.error("Traces not imported. Invalid File? %s" % basename)
-           continue
+		# Prepare analysis
+		analyzer = TreeAnalyzer(t)
+		analyzer.setContext(context)
+		analyzer.setTable(table, ("SWC Measurements: %s" % input_dir))
 
-        # Prepare analysis
-        analyzer = TreeAnalyzer(tree)
-        analyzer.setContext(context)
-        analyzer.setTable(table, ("SWC Measurements: %s" % input_dir))
+		# Analyze the data grouping measurements by compartment (e.g., axon,
+		# dendrite). See TreeAnalyzer's API for more sophisticated analysis.
+		# Table is automatically updated
+		analyzer.summarize(True) # Split results by compartment?
 
-        # Analyze the data grouping measurements by compartment (e.g., axon,
-        # dendrite). See TreeAnalyzer's API for more sophisticated analysis.
-        # Table is automatically updated
-        analyzer.summarize(True) # Split results by compartment?
-
-    log.info('Done. %s file(s) analyzed...' % (counter + 1))  
-    if table.getRowCount() == 0:
-        uiservice.showDialog("No files were analyzed", "Error")
-        
+	log.info('Done. %s file(s) analyzed...' % (counter + 1))  
+	if table.getRowCount() == 0:
+		uiservice.showDialog("No files were analyzed", "Error")
+		
 
 run()

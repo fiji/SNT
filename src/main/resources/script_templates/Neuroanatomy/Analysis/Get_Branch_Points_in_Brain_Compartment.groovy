6eb7b53806177ca2429538329597bd6a63ca6503
tferr <tiago.tferr@gmail.com>
2020-02-24 00:24:37 -0500

    Update script

--- src/main/resources/script_templates/Neuroanatomy/Analysis/Get_Branch_Points_in_Brain_Compartment.groovy
+++ src/main/resources/script_templates/Neuroanatomy/Analysis/Get_Branch_Points_in_Brain_Compartment.groovy
@@ -19,79 +19,47 @@ import org.scijava.util.*
  */
 
 
-// First we specify the compartments of the cells we want to filter
-// (we'll use the name of the ARA compartments to be screened as
-// displayed by Reconstruction Viewer's CCF Navigator)
+// First we specify the id of the cell we are going to parse and the brain
+// compartments we are interested in:
+cellId = "AA0596"
 somaCompartment = AllenUtils.getCompartment("Somatomotor areas")
 thalamus = AllenUtils.getCompartment("Thalamus")
 
-/** 
- * Extracts all the branch points (BPs) of the specified cell that are
- * contained by {@code thalamus}. Does nothing if cell's soma is not
- * contained by {@code somaCompartment}
- */
-def getFilteredBranchPoints(cellId) {
-
-	if (!somaCompartment || !thalamus) {
-		println("Aborting: Compartment(s) are not valid")
-		return null
-	}
-	loader = new MouseLightLoader(cellId)
-	print("Parsing " + cellId + "...")
-	if (!loader.idExists()) {
-		println(" id not found. Skipping...")
-		return null
-	}
-	// Retrieve the 1st node of the soma and its annotated compartment
-	soma = loader.getNodes("soma")[0]
-	compartment = (AllenCompartment) soma.getAnnotation()
-	if (!somaCompartment.contains(compartment)) {
-		println(" Soma not associated with " + somaCompartment + ". Skipping...")
-		return null
-	}
-	println(" Id matches soma location requirements!")
-
-	// Retrieve the axonal arbor as a Tree object. Instantiate a TreeAnalyzer
-	// so that we can conveniently access all of the axonal branch points
-	axonalTree = loader.getTree("axon")
-	analyzer = new TreeAnalyzer(axonalTree)
-	branchPoints = analyzer.getBranchPoints()
-
-	// Iterate through all the branch points (PointInImage objects)
-	// and extract those in thalamus
-	print("Assessing location of ${branchPoints.size()} branch points...")
-	filteredBranchPoints = []
-	for (branchPoint in branchPoints) {
-		compartment = (AllenCompartment) branchPoint.getAnnotation()
-		if (compartment) //&& thalamus.contains(compartment))
-			filteredBranchPoints.add(branchPoint);
-	}
-	println(" Found ${filteredBranchPoints.size()} match(es)")
-
-	// return filtered list of branch points
-	filteredBranchPoints
+// First we define a Loader for the MouseLightDatabase:
+loader = new MouseLightLoader(cellId)
+if (!loader.idExists()) {
+	println(" id not found. Aborting...")
+	return null
 }
 
-println("Analysis settings are: ")
-println(" Soma compartment: $somaCompartment")
-println(" BPs compartment: $thalamus")
-
-// In this demo we'll just parse a single cell. Have a look at
-// Download_ML_Data.groovy for an example on how to parse all
-// neurons in the ML database
-id = "AA0596"
-bps = getFilteredBranchPoints(id)
+// Now we extract all the axonal branch points in the Thalamus.
+// See method below for detaisl:
+bps = getFilteredBranchPoints(loader)
 if (!bps) {
   println(" Bummer! No branch points are associated with $thalamus")
   return
 }
 
-// Let's look at the centroid of the extracted nodes
+// Let's look at the soma location of the analyzed cell, and the centroid
+// defined by the extracted branch points. 
+somaCentroid = SNTPoint.average(loader.getNodes("soma"))
 bpsCentroid = SNTPoint.average(bps)
-somaNodes = new MouseLightLoader(id).getNodes("soma")
-somaCentroid = SNTPoint.average(somaNodes)
 println("SomaLocation: $somaCentroid.x, $somaCentroid.y, $somaCentroid.z")
-println("Centroid of all BPs in ${thalamus.name()}: $bpsCentroid.x, $bpsCentroid.y, $bpsCentroid.z")
+println("Centroid of BPs in ${thalamus.name()}: $bpsCentroid.x, $bpsCentroid.y, $bpsCentroid.z")
+
+// Let's find out how many of the branch points are in the left hemisphere
+// (NB: In the ML database the medial-Lateral axis is mapped to the X-axis)
+midline = AllenUtils.brainCenter().getX()
+nodeStats = new NodeStatistics(bps, loader.getTree("axon"))
+hist = nodeStats.getHistogram("x coordinates")
+hist.annotateXline(midline, "midline")
+hist.annotatePoint(somaCentroid.getX(), 0, "soma")
+hist.show()
+
+// Let's retrieve some details about the branches associated with the
+// extracted branch points:
+hist = nodeStats.getHistogram("branch order")
+hist.show()
 
 // Let's assemble a Reconstruction Viewer to visualize the data
 viewer = new Viewer3D(ij.context())
@@ -107,10 +75,9 @@ tree = loader.getTree()
 tree.setColor("yellow")
 viewer.add(tree)
 
-// Annotate thalamic BPs color coded by depth and
-// add them to viewer as point-cloud annotation
-mapper = new TreeColorMapper(ij.context())
-mapper.map(bps, "z coordinates", "Ice.lut")
+// Add extracted branch points color-coded by order
+mapper = new NodeColorMapper(nodeStats, ij.context())
+mapper.map("branch length", "Ice.lut")
 annot = viewer.annotatePoints(bps, "Thalamic BPs")
 annot.setSize(10)
 
@@ -127,3 +94,41 @@ for (annot in miscAnnotations) {
 viewer.show()
 viewer.setAnimationEnabled(true)
 print("... Done. With Viewer active, Press 'H' or 'F1' for help")
+
+
+/** 
+ * Extracts all the branch points (BPs) of the specified cell that are
+ * contained by {@code thalamus}. Does nothing if cell's soma is not
+ * contained by {@code somaCompartment}
+ */
+def getFilteredBranchPoints(loader) {
+
+	if (!somaCompartment || !thalamus) {
+		println("Aborting: Compartment(s) are not valid")
+		return null
+	}
+	if (!loader.idExists()) {
+		println(" id not found. Skipping...")
+		return null
+	}
+	// Retrieve the 1st node of the soma and its annotated compartment
+	soma = loader.getNodes("soma")[0]
+	compartment = (AllenCompartment) soma.getAnnotation()
+	if (!somaCompartment.contains(compartment)) {
+		println(" Soma not associated with " + somaCompartment + ". Skipping...")
+		return null
+	}
+	println(" Id matches soma location requirements!")
+
+	// Retrieve the axonal arbor as a Tree object. Instantiate a TreeAnalyzer
+	// so that we can conveniently 1) access all of the axonal branch points,
+	// and 2) filter them by annotated compartment
+	axonalTree = loader.getTree("axon")
+	analyzer = new TreeAnalyzer(axonalTree)
+	branchPoints = analyzer.getBranchPoints(thalamus)
+	println(" Found ${branchPoints.size()} match(es)")
+
+	// return the filtered list of branch points
+	branchPoints
+}
+

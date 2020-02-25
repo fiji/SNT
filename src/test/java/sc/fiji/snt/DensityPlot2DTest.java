84cef4ecfcf6723cda541e1b09c37dc34cef45f7
Kyle IS Harrington <kyle.i.s.harrington@gmail.com>
2020-02-25 14:28:01 -0500

    Density plot2d updated

---
+++ src/test/java/sc/fiji/snt/DensityPlot2DTest.java
@@ -0,0 +1,180 @@
+package sc.fiji.snt;
+
+import ij.IJ;
+import ij.ImagePlus;
+import net.imglib2.Cursor;
+import net.imglib2.FinalInterval;
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.img.Img;
+import net.imglib2.img.display.imagej.ImageJFunctions;
+import net.imglib2.type.numeric.integer.LongType;
+import net.imglib2.type.numeric.integer.UnsignedByteType;
+import net.imglib2.type.numeric.integer.UnsignedShortType;
+import net.imglib2.type.numeric.real.FloatType;
+import net.imglib2.view.IntervalView;
+import net.imglib2.view.Views;
+import org.apache.commons.math3.stat.descriptive.SummaryStatistics;
+import sc.fiji.snt.util.PointInImage;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DensityPlot2DTest {
+    static public void densityPlot(String swcDirectory, String outDirectory, String resultPrefix) throws IOException {
+        List<Tree> trees = Tree.listFromDir(swcDirectory);
+
+        System.out.println("Processing: " + swcDirectory);
+
+        int[] maxDims = new int[]{0, 0};
+
+        new ij.ImageJ();
+
+        int numTreesToEstimateOutDimensions = trees.size();
+        //int numTreesToEstimateOutDimensions = 5;
+
+        // ----- Guess the output dims
+
+        long[] outputDims = new long[]{0, 0};
+
+        double[] outputPadding = new double[]{3, 3};
+        double[] outputMin = new double[]{Double.MAX_VALUE, Double.MAX_VALUE};
+        double[] outputMax = new double[]{Double.MIN_VALUE, Double.MIN_VALUE};
+
+        for( Tree tree : trees.subList(0, numTreesToEstimateOutDimensions )) {
+            System.out.println("Processing tree: " + tree);
+//            # retrieve an empty image capable of hosting the rasterized tree
+
+            // Use soma to align
+            //tree.getRoot();
+
+            ImagePlus imp = tree.getSkeleton2D();
+
+            PointInImage r = tree.getRoot();
+
+            final SummaryStatistics xStats = new SummaryStatistics();
+            final SummaryStatistics yStats = new SummaryStatistics();
+            for (final PointInImage p : tree.getNodes()) {
+                xStats.addValue(p.x);
+                yStats.addValue(p.y);
+            }
+            final PointInImage mn = new PointInImage(xStats.getMin(), yStats.getMin(), 0);
+            final PointInImage mx = new PointInImage(xStats.getMax(), yStats.getMax(), 0);
+
+            outputMin[0] = Math.min(outputMin[0], (mn.x - r.x) - outputPadding[0]);
+            outputMin[1] = Math.min(outputMin[1], (mn.y - r.y) - outputPadding[1]);
+
+            outputMax[0] = Math.max(outputMax[0], (mx.x - r.x) + outputPadding[0]);
+            outputMax[1] = Math.max(outputMax[1], (mx.y - r.y) + outputPadding[1]);
+
+        }
+
+        // Pad the answer
+        for( int d = 0; d < 2; d++ ) {
+            outputDims[d] = (long) (outputMax[d] - outputMin[d]);
+        }
+
+        // ----- Done guessing output dims
+
+        System.out.println("maxDims: " + outputDims[0] + " " + outputDims[1]);
+
+        // make an image of the largest size
+        ImagePlus outImp = IJ.createImage("outImg", "float32", (int) outputDims[0], (int) outputDims[1], 1);
+
+        RandomAccessibleInterval<FloatType> outImg =
+                Views.translate((RandomAccessibleInterval<FloatType>)ImageJFunctions.wrapReal(outImp), (long) outputMin[0], (long) outputMin[1]);
+
+        System.out.println("outImg: " + outImg);
+
+        List<PointInImage> origins = new ArrayList<>();
+
+        for( int k = 0; k < trees.size(); k++ ) {
+            Tree tree = trees.get(k);
+
+            System.out.println("Processing tree: " + k + " " + tree);
+
+            ImagePlus imp = tree.getSkeleton2D();
+
+            PointInImage r = tree.getRoot();
+
+            final SummaryStatistics xStats = new SummaryStatistics();
+            final SummaryStatistics yStats = new SummaryStatistics();
+            for (final PointInImage p : tree.getNodes()) {
+                xStats.addValue(p.x);
+                yStats.addValue(p.y);
+            }
+            final PointInImage mn = new PointInImage(xStats.getMin(), yStats.getMin(), 0);
+            final PointInImage mx = new PointInImage(xStats.getMax(), yStats.getMax(), 0);
+
+            double[] imMin = new double[2];
+            double[] imMax = new double[2];
+
+            imMin[0] = mn.x - r.x;
+            imMin[1] = mn.y - r.y;
+
+            imMax[0] = mx.x - r.x;
+            imMax[1] = mx.y - r.y;
+
+            Img<UnsignedByteType> im = ImageJFunctions.wrap(imp);
+
+            System.out.println("Plotting " + tree );
+            System.out.println("Root: " + r.x + " " + r.y);
+            System.out.println("Img size: " + im.dimension(0) + " " + im.dimension(1));
+
+            FinalInterval outInterval = new FinalInterval(
+                    new long[]{(long) ((long) imMin[0] - outputPadding[0]), (long) ((long) imMin[1] - outputPadding[1])},
+                    new long[]{(long) ((long) imMax[0] + outputPadding[0]), (long) ((long) imMax[1] + outputPadding[0])});
+
+            System.out.println("Out interval: " + outInterval);
+
+            IntervalView<FloatType> outView = Views.interval(outImg, outInterval);
+
+            Cursor<UnsignedByteType> inCur = Views.flatIterable(im).cursor();
+            Cursor<FloatType> outCur = Views.flatIterable(outView).cursor();
+            while( inCur.hasNext() ) {
+                inCur.fwd();
+                outCur.fwd();
+                outCur.get().set( outCur.get().get() + inCur.get().get() );
+            }
+
+            if( k < 5 ) {
+                ImagePlus frameImp = IJ.createImage("outImg", "float32", (int) outputDims[0], (int) outputDims[1], 1);
+                RandomAccessibleInterval<FloatType> frameImg =
+                        Views.translate((RandomAccessibleInterval)ImageJFunctions.wrapReal(frameImp), (long) outputMin[0], (long) outputMin[1]);
+                IntervalView<FloatType> frameView = Views.interval(frameImg, outInterval);
+                //IntervalView<FloatType> frameView = Views.interval(frameImg, Views.translate(im, offset));
+
+                System.out.println("frameView: " + frameView);
+
+                inCur = Views.flatIterable(im).cursor();
+                Cursor<FloatType> frameCur = Views.flatIterable(frameView).cursor();
+
+                while( inCur.hasNext() ) {
+                    inCur.fwd();
+                    frameCur.fwd();
+                    frameCur.get().set( inCur.get().get() );
+                }
+
+                IJ.saveAsTiff(frameImp, resultPrefix + "density_plot2d_example_" + k + ".tif");
+            }
+
+        }
+
+//        ImagePlus outImp = ImageJFunctions.wrap(outImg, "densityPlot");
+        IJ.saveAsTiff(outImp, resultPrefix + "density_plot2d.tif");
+
+    }
+
+    static public void main(String[] args) throws IOException {
+        //String parentDirectory = System.getProperty("user.home") + "/Dropbox/SNTmanuscript/Simulations/GRNFinalAnalysis";
+        String parentDirectory = System.getProperty("user.home") + "/Data/SNT/GRN_RandomNeuriteDir";
+        String resultDirectory = parentDirectory + "/output";
+
+        densityPlot(parentDirectory + "/grn0/", parentDirectory + "/grn0img/", resultDirectory + "/grn0_");
+        densityPlot(parentDirectory + "/grn1/", parentDirectory + "/grn1img/", resultDirectory + "/grn1_");
+        densityPlot(parentDirectory + "/grn2/", parentDirectory + "/grn2img/", resultDirectory + "/grn2_");
+        densityPlot(parentDirectory + "/grn3/", parentDirectory + "/grn3img/", resultDirectory + "/grn3_");
+        densityPlot(parentDirectory + "/grn4/", parentDirectory + "/grn4img/", resultDirectory + "/grn4_");
+    }
+
+}

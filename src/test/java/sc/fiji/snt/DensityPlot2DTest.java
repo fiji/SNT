6aff7c7060e66a8fa388988318613a5728a31e08
Kyle IS Harrington <kyle.i.s.harrington@gmail.com>
2020-02-26 10:06:10 -0500

    Fixed density plot dimensions mismatch that caused skewing

--- src/test/java/sc/fiji/snt/DensityPlot2DTest.java
+++ src/test/java/sc/fiji/snt/DensityPlot2DTest.java
@@ -1,11 +1,14 @@
 package sc.fiji.snt;
 
 import ij.IJ;
+import ij.ImageJ;
 import ij.ImagePlus;
 import net.imglib2.Cursor;
 import net.imglib2.FinalInterval;
+import net.imglib2.Interval;
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.img.Img;
+import net.imglib2.img.array.ArrayImgs;
 import net.imglib2.img.display.imagej.ImageJFunctions;
 import net.imglib2.type.numeric.integer.LongType;
 import net.imglib2.type.numeric.integer.UnsignedByteType;
@@ -18,6 +21,7 @@ import sc.fiji.snt.util.PointInImage;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class DensityPlot2DTest {
@@ -64,8 +68,8 @@ public class DensityPlot2DTest {
             outputMin[0] = Math.min(outputMin[0], (mn.x - r.x) - outputPadding[0]);
             outputMin[1] = Math.min(outputMin[1], (mn.y - r.y) - outputPadding[1]);
 
-            outputMax[0] = Math.max(outputMax[0], (mx.x - r.x) + outputPadding[0]);
-            outputMax[1] = Math.max(outputMax[1], (mx.y - r.y) + outputPadding[1]);
+            outputMax[0] = Math.max(outputMax[0], (mx.x - r.x) + outputPadding[0] + 1);
+            outputMax[1] = Math.max(outputMax[1], (mx.y - r.y) + outputPadding[1] + 1);
 
         }
 
@@ -79,10 +83,12 @@ public class DensityPlot2DTest {
         System.out.println("maxDims: " + outputDims[0] + " " + outputDims[1]);
 
         // make an image of the largest size
-        ImagePlus outImp = IJ.createImage("outImg", "float32", (int) outputDims[0], (int) outputDims[1], 1);
+        //ImagePlus outImp = IJ.createImage("outImg", "float32", (int) outputDims[0], (int) outputDims[1], 1);
+
+        RandomAccessibleInterval<FloatType> sourceImg = ArrayImgs.floats(outputDims[0], outputDims[1]);
 
         RandomAccessibleInterval<FloatType> outImg =
-                Views.translate((RandomAccessibleInterval<FloatType>)ImageJFunctions.wrapReal(outImp), (long) outputMin[0], (long) outputMin[1]);
+                Views.translate(sourceImg, (long) outputMin[0], (long) outputMin[1]);
 
         System.out.println("outImg: " + outImg);
 
@@ -123,7 +129,7 @@ public class DensityPlot2DTest {
 
             FinalInterval outInterval = new FinalInterval(
                     new long[]{(long) ((long) imMin[0] - outputPadding[0]), (long) ((long) imMin[1] - outputPadding[1])},
-                    new long[]{(long) ((long) imMax[0] + outputPadding[0]), (long) ((long) imMax[1] + outputPadding[0])});
+                    new long[]{(long) ((long) imMax[0] + outputPadding[0]), (long) ((long) imMax[1] + outputPadding[1])});
 
             System.out.println("Out interval: " + outInterval);
 
@@ -138,30 +144,53 @@ public class DensityPlot2DTest {
             }
 
             if( k < 5 ) {
-                ImagePlus frameImp = IJ.createImage("outImg", "float32", (int) outputDims[0], (int) outputDims[1], 1);
+                //ImagePlus frameImp = IJ.createImage("outImg", "float32", (int) outputDims[0], (int) outputDims[1], 1);
+                sourceImg = ArrayImgs.floats(outputDims[0], outputDims[1]);
                 RandomAccessibleInterval<FloatType> frameImg =
-                        Views.translate((RandomAccessibleInterval)ImageJFunctions.wrapReal(frameImp), (long) outputMin[0], (long) outputMin[1]);
+                        Views.translate(sourceImg, (long) (outputMin[0] + outputPadding[0]), (long) (outputMin[1] + outputPadding[1]));
+
+                outInterval = new FinalInterval(
+                    new long[]{(long) ((long) imMin[0]), (long) ((long) imMin[1])},
+                    new long[]{(long) ((long) imMin[0] + im.dimension(0) - 1), (long) ((long) imMin[1] + im.dimension(1) - 1)});
+
                 IntervalView<FloatType> frameView = Views.interval(frameImg, outInterval);
                 //IntervalView<FloatType> frameView = Views.interval(frameImg, Views.translate(im, offset));
 
+                System.out.println("outInterval: " + outInterval);
                 System.out.println("frameView: " + frameView);
+                System.out.println((Interval) im);
 
-                inCur = Views.flatIterable(im).cursor();
-                Cursor<FloatType> frameCur = Views.flatIterable(frameView).cursor();
+                inCur = Views.iterable(im).cursor();
+                Cursor<FloatType> frameCur = Views.iterable(frameView).cursor();
+
+                long[] pos = new long[2];
 
                 while( inCur.hasNext() ) {
                     inCur.fwd();
                     frameCur.fwd();
-                    frameCur.get().set( inCur.get().get() );
+
+//                    inCur.localize(pos);
+//                    System.out.print("incur: " + Arrays.toString(pos) + " ");
+//                    frameCur.localize(pos);
+//                    System.out.println("framecur: " + Arrays.toString(pos));
+
+                    frameCur.get().set( inCur.get().get() + 10 );
                 }
 
-                IJ.saveAsTiff(frameImp, resultPrefix + "density_plot2d_example_" + k + ".tif");
+                ImagePlus frameImp = ImageJFunctions.wrap(frameImg, "frame");
+                frameImp.show();
+                imp.show();
+
+                ImageJFunctions.wrap(im, "wrappedim").show();
+                ImageJFunctions.wrap(frameView, "wrappedframe").show();
+
+                IJ.saveAsTiff(frameImp, resultPrefix + "density_plot2d_example_" + k + "_" + tree.getLabel() + ".tif");
             }
 
         }
 
 //        ImagePlus outImp = ImageJFunctions.wrap(outImg, "densityPlot");
-        IJ.saveAsTiff(outImp, resultPrefix + "density_plot2d.tif");
+        IJ.saveAsTiff(ImageJFunctions.wrap(outImg, "densityPlot"), resultPrefix + "density_plot2d.tif");
 
     }
 
@@ -170,11 +199,18 @@ public class DensityPlot2DTest {
         String parentDirectory = System.getProperty("user.home") + "/Data/SNT/GRN_RandomNeuriteDir";
         String resultDirectory = parentDirectory + "/output";
 
-        densityPlot(parentDirectory + "/grn0/", parentDirectory + "/grn0img/", resultDirectory + "/grn0_");
+        ImageJ ij = new ImageJ();
+
+//        Tree t = new Tree("/home/kharrington/Dropbox/SNTmanuscript/Simulations/GRN_RandomNeuriteDirection/snt_maxTime_6_filenameGRN_grn_1.grn_randomSeed_20326147.swc");
+//        t.getSkeleton2D().show();
+
+        //densityPlot(parentDirectory + "/grn0/", parentDirectory + "/grn0img/", resultDirectory + "/grn0_");
         densityPlot(parentDirectory + "/grn1/", parentDirectory + "/grn1img/", resultDirectory + "/grn1_");
-        densityPlot(parentDirectory + "/grn2/", parentDirectory + "/grn2img/", resultDirectory + "/grn2_");
-        densityPlot(parentDirectory + "/grn3/", parentDirectory + "/grn3img/", resultDirectory + "/grn3_");
-        densityPlot(parentDirectory + "/grn4/", parentDirectory + "/grn4img/", resultDirectory + "/grn4_");
+//        densityPlot(parentDirectory + "/grn2/", parentDirectory + "/grn2img/", resultDirectory + "/grn2_");
+//        densityPlot(parentDirectory + "/grn3/", parentDirectory + "/grn3img/", resultDirectory + "/grn3_");
+//        densityPlot(parentDirectory + "/grn4/", parentDirectory + "/grn4img/", resultDirectory + "/grn4_");
+
+
     }
 
 }
